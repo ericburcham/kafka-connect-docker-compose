a5d3e79ccc84cbd985e808c8b96005290b51a322
Eric Burch <jameseburcham@gmail.com>
2022-08-18 02:02:57 -0500

    update init and run

--- init-and-run.ps1
+++ init-and-run.ps1
@@ -1,9 +1,6 @@
 # Configure git flow
 git flow init -d --feature feature/ --bugfix bugfix/ --release release/ --hotfix hotfix/ --support support/ -t 'v';
 
-# Checkout develop
-git checkout develop;
-
 # Check if the NUKE global tool is already installed.
 Write-Output "Checking for NUKE global tool."
 if ((dotnet tool list -g).Where({ $_ -like "*nuke.globaltool*"}, 'First').Count -gt 0)
