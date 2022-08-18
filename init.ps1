121442b68b80bb2c179a252a351fb2175ee96e5a
Eric Burch <jameseburcham@gmail.com>
2022-08-18 00:11:34 -0500

    start init script for installing the NUKE global tool

---
+++ init.ps1
@@ -0,0 +1,12 @@
+# Check if the NUKE global tool is already installed.
+Write-Output "Checking for NUKE global tool."
+if ((dotnet tool list -g).Where({ $_ -like "*nuke.globaltool*"}, 'First').Count -gt 0)
+{
+    Write-Output "NUKE global tool found.  Skipping installation."
+}
+else
+{
+    # Install NUKE Build globally.
+    Write-Output "NUKE global tool not found.  Installing."
+    dotnet tool install Nuke.GlobalTool --global
+}
